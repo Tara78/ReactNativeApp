68f220e205a38797b6471f23e6f369c18b3f1057
Tara Hassani <70331037+Tara78@users.noreply.github.com>
2023-08-26 16:51:22 +0200

    .

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -34,7 +34,7 @@ const styles= StyleSheet.create({
         flexDirection: "row",
         justifyContent: "space-between",
         alignItems: "center",
-        marginBottom: 24,
+        marginBottom: 30,
         borderBottomWidth: 1,
         borderBottomColor: "#cccc",
       },
